9dd8ebd9bb6415ccbb7d8f769941abdf1151fc76
Derric Williams <derricw@gmail.com>
2024-12-19 08:35:07 -0800

    streams query working but not displaying

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	//"log"
 	"os"
 
-	//"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
@@ -21,6 +21,7 @@ type mode int
 
 const (
 	Groups mode = iota
+	Streams
 	Page
 )
 
@@ -39,7 +40,20 @@ func getLogGroups() tea.Msg {
 	return logGroupMsg(logGroups)
 }
 
+func getLogStreams(logGroupName string) tea.Msg {
+	client, err := createClient()
+	if err != nil {
+		return errMsg{err}
+	}
+	logStreams, err := fetchLogStreams(client, logGroupName)
+	if err != nil {
+		return errMsg{err}
+	}
+	return logStreamMsg(logStreams)
+}
+
 type logGroupMsg []types.LogGroup
+type logStreamMsg []types.LogStream
 
 type errMsg struct{ err error }
 
@@ -56,11 +70,14 @@ func (i item) Description() string { return i.desc }
 func (i item) FilterValue() string { return i.title }
 
 type model struct {
-	list          list.Model
-	viewport      viewport.Model
-	logGroups     []types.LogGroup
-	groupSelected list.Item
-	mode          mode
+	groupsList     list.Model
+	streamsList    list.Model
+	viewport       viewport.Model
+	logGroups      []types.LogGroup
+	logStreams     []types.LogStream
+	groupSelected  list.Item
+	streamSelected list.Item
+	mode           mode
 }
 
 func (m model) Init() tea.Cmd {
@@ -69,27 +86,6 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		k := msg.String()
-		switch k {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			if !m.list.SettingFilter() {
-				m.groupSelected = m.list.SelectedItem()
-				m.mode = Page
-				return m, nil
-			}
-		case "esc":
-			if m.mode == Page {
-				m.groupSelected = nil
-				m.mode = Groups
-				return m, nil
-			}
-		}
-	case tea.WindowSizeMsg:
-		h, v := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
 	case logGroupMsg:
 		items := []list.Item{}
 		for _, group := range msg {
@@ -98,26 +94,121 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				desc:  *group.LogGroupArn,
 			})
 		}
-		m.list.SetItems(items)
+		m.groupsList.SetItems(items)
 		m.logGroups = msg
+	case logStreamMsg:
+		items := []list.Item{}
+		for _, stream := range msg {
+			items = append(items, item{
+				title: *stream.LogStreamName,
+			})
+		}
+		m.streamsList.SetItems(items)
+		m.logStreams = msg
+	}
+
+	switch m.mode {
+	case Groups:
+		return m.updateGroups(msg)
+	case Streams:
+		return m.updateStreams(msg)
+	case Page:
+		return m.updatePage(msg)
+	}
+	var cmd tea.Cmd
+	return m, cmd
+}
+
+func (m model) updateGroups(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		k := msg.String()
+		switch k {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+		case "enter":
+			if !m.groupsList.SettingFilter() {
+				m.groupSelected = m.groupsList.SelectedItem()
+				m.mode = Streams
+				return m, func() tea.Msg {
+					selectedLogGroup := m.logGroups[m.groupsList.Index()]
+					return getLogStreams(*selectedLogGroup.LogGroupName)
+				}
+			}
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.groupsList.SetSize(msg.Width-h, msg.Height-v)
+	}
+	var cmd tea.Cmd
+	m.groupsList, cmd = m.groupsList.Update(msg)
+	return m, cmd
+
+}
+
+func (m model) updateStreams(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		k := msg.String()
+		switch k {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			if !m.streamsList.SettingFilter() {
+				m.streamSelected = m.streamsList.SelectedItem()
+				m.mode = Page
+				return m, nil
+			}
+		case "esc":
+			m.mode = Groups
+			return m, nil
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.streamsList.SetSize(msg.Width-h, msg.Height-v)
 	}
 
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	m.streamsList, cmd = m.streamsList.Update(msg)
+	return m, cmd
+}
+
+func (m model) updatePage(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		k := msg.String()
+		switch k {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "esc":
+			m.mode = Streams
+			return m, nil
+		}
+	}
+	var cmd tea.Cmd
+	m.viewport, cmd = m.viewport.Update(msg)
 	return m, cmd
 }
 
 func (m model) View() string {
 	if m.mode == Groups {
-		return docStyle.Render(m.list.View())
+		return docStyle.Render(m.groupsList.View())
+	} else if m.mode == Streams {
+		return docStyle.Render(m.streamsList.View())
+	} else {
+		return docStyle.Render(m.viewport.View())
 	}
-	return docStyle.Render(m.viewport.View())
 }
 
 func initialModel() model {
-	items := []list.Item{}
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "Log Groups"
+	groups, streams := []list.Item{}, []list.Item{}
+	m := model{
+		groupsList:  list.New(groups, list.NewDefaultDelegate(), 0, 0),
+		streamsList: list.New(streams, list.NewDefaultDelegate(), 0, 0),
+		viewport:    viewport.New(50, 50),
+	}
+	m.groupsList.Title = "Log Groups"
+	m.streamsList.Title = "Log Streams"
 	return m
 }
 
@@ -135,6 +226,18 @@ func fetchLogGroups(client *cloudwatchlogs.Client) ([]types.LogGroup, error) {
 		return nil, err
 	}
 	return output.LogGroups, nil
+}
+
+func fetchLogStreams(client *cloudwatchlogs.Client, logGroupName string) ([]types.LogStream, error) {
+	output, err := client.DescribeLogStreams(context.TODO(), &cloudwatchlogs.DescribeLogStreamsInput{
+		LogGroupName: &logGroupName,
+		Limit:        aws.Int32(50),
+		OrderBy:      types.OrderByLastEventTime,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return output.LogStreams, nil
 }
 
 func main() {
